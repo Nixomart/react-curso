b476dff9478b56c0df06fa28aedea905cebee55a
Nixomart <80935886+Nixomart@users.noreply.github.com>
2022-03-10 18:54:00 -0300

    agregado de html2canvas y pegado de img en el mism

--- src/App.js
+++ src/App.js
@@ -1,63 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-//el nombre de la funcion debe ir a corde por lo general al nombre del archivo app
+import html2canvas from 'html2canvas';
 function App() {
 
-  //const for set to span, setlinea1 se va setear usando el useState si ponemos 
-  //algo ahi se va poner en el array1 osea linea1
-const [linea1,setLinea1] = useState('');
-const [linea2,setLinea2] = useState('');
+//manejo de estados
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [img, setImg] = useState('');
 
-//funcion constante para abrir el evento en un aler este evento sera el input que 
-//agreguemos, tendremos que gregar sus metodos para que nos muestre el valor
+//manejo de funciones con los estados declarados
+  const onChangeLinea1 = function (evento) {
+    setLinea1(evento.target.value);
+  }
+  const onChangeLinea2 = function (evento) {
+    setLinea2(evento.target.value);
+  }
+  const onChangeImg = function (evento) {
+    setImg(evento.target.value);
+  }
+  //funcion para el boton exportar importando libreria de html2canvas
+  //captura la imagen y en este caso la pega de neuvo en el html
+  const onClickExport = function (evento) {
 
-//escribiendo setLinea1 llamos a la funcion set que devolvera lo que en el input 
-//este escrito y react lo toma como un componente que se estara actualizando
-//a cada rato el html entonces podemos escribir libremente
-const onChangeLinea1 = function(evento){
-  setLinea1(evento.target.value);
-}
-const onChangeLinea2 = function(evento){
-  setLinea2(evento.target.value);
-}
+    //funcion flecha canvas que se va a ejecturar cuando se haya procesado todo lo anterior
+    html2canvas(document.querySelector("#capture")).then(canvas => {
+      document.body.appendChild(canvas)
+  });
+  }
 
-  //retorna codigo html entre ()
+
   return (
     <div className="App">
-      
-      {// selec memes img 
-      }
-      <select>
-        <option value="meme1">plantilla 1</option>
-        <option value="meme2">plantilla 2</option>
+
+     
+      <select onChange={onChangeImg}>
+        {// el value es el nombre de la imagen sin el png para que concatenee bien en la 
+        //funcion onChange
+        }
+        <option value="gustavo">plantilla 1</option>
+        <option value="tipoPensando">plantilla 2</option>
       </select><br />
 
-      
-      
-      {//input for first line
-      } 
       <input onChange={onChangeLinea1} type="text" placeholder="linea 1 de meme"></input><br />
-      
-      
-      
-      {//input for second line
-      } 
       <input onChange={onChangeLinea2} type="text" placeholder="linea 2 de meme"></input><br />
-      
-      
-      
-      
-      {//button's export
-      }
-      <button >Exportar momo :V </button>
-      
-      
-      {//span for the lines written in the inputs
-      } 
-      <div>
+
+      <button onClick={onClickExport}>Exportar momo :V </button>
+
+      <div className="flex-div">
         <span>{linea1}</span><br />
         <span>{linea2}</span>
+        <img src={"/img/"+ img +".jpg"}></img>
       </div>
 
     </div>
